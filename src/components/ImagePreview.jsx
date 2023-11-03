ee69cd6770bdf6cb19f1d297a649692c5afc6fed
Pe3k <jakub53k@gmail.com>
2023-11-03 11:47:59 +0100

    links to preview added

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -1,14 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export const ImagePreview = ({ image }) => {
   return (
     <div className="ImagePreview">
-      <div className="imagePart">
-        <img src={image.urls.small} alt={image.alt_description} />
-      </div>
-      <div className="authorPart">
-        <span className="author__name">{image.user.name}</span>
-      </div>
+      <Link to={`/photos/${image.id}`}>
+        <div className="imagePart">
+          <img src={image.urls.small} alt={image.alt_description} />
+        </div>
+      </Link>
+      <Link to={`/author/${image.user.id}`}>
+        <div className="authorPart">
+          <span className="author__name">{image.user.name}</span>
+        </div>
+      </Link>
     </div>
   );
 };
