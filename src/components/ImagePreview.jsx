3cfa4a44c65f11125ef463c2dbc6be6ea0ac8924
EyK-26 <137394243+EyK-26@users.noreply.github.com>
2023-11-03 13:09:33 +0100

    correct Jakub's Link

--- src/components/ImagePreview.jsx
+++ src/components/ImagePreview.jsx
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 export const ImagePreview = ({ image }) => {
   return (
     <div className="ImagePreview">
-      <Link to={`/photos/${image.id}`}>
+      <Link to={`/image/${image.id}`}>
         <div className="imagePart">
           <img src={image.urls.small} alt={image.alt_description} />
         </div>
