71d1bfc6f884906d633bd0a0d5cf210a6dc93ba5
Ondrej.Kutina <onakutin@protonmail.com>
2023-11-03 13:14:21 +0100

    author finished

--- src/router/Router.jsx
+++ src/router/Router.jsx
@@ -7,18 +7,18 @@ import { Author } from "../components/Author";
 import { Image } from "../components/Image";
 
 export const Router = () => {
-  const { state } = useContext(Context);
+	const { state } = useContext(Context);
 
-  return (
-    <div className={state.theme === "light" ? "container_light" : undefined}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<HomePage />} />
-          <Route path="/author/:id" element={<Author />} />
-          <Route path="/image/:id" element={<Image />} />
-          <Route path="*" element="404 page not found" />
-        </Route>
-      </Routes>
-    </div>
-  );
+	return (
+		<div className={state.theme === "light" ? "container_light" : undefined}>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<HomePage />} />
+					<Route path="/author/:id" element={<Author />} />
+					<Route path="/image/:id" element={<Image />} />
+					<Route path="*" element="404 page not found" />
+				</Route>
+			</Routes>
+		</div>
+	);
 };
