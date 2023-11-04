d2b47e9b569b2b92a477b855ab52bc14a0cecab7
Ondrej.Kutina <onakutin@protonmail.com>
2023-11-04 08:43:24 +0100

    footer at the bottom

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,24 +1,29 @@
-import { useState } from "react"
+import { useState } from "react";
 
 export const SearchBar = ({ setSearch }) => {
-
-  const [input, setInput] = useState('')
+  const [input, setInput] = useState("");
 
   const handleSearch = (event) => {
-    setInput(event.target.value)
-  }
+    setInput(event.target.value);
+  };
 
   const submitSearch = (event) => {
-    event.preventDefault()
-    setSearch(input)
-  }
+    event.preventDefault();
+    setSearch(input);
+  };
 
   return (
     <div className="SearchBar">
       <form onSubmit={submitSearch}>
-        <input className="input input__text" type="text" placeholder="Search for a image..." onInput={handleSearch} value={input} />
+        <input
+          className="input input__text"
+          type="text"
+          placeholder="Search for an image....."
+          onInput={handleSearch}
+          value={input}
+        />
         <button type="submit"> Search </button>
       </form>
     </div>
-  )
-}
+  );
+};
--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -2,14 +2,21 @@ import { Outlet, useNavigate } from "react-router-dom";
 import { Header } from "../components/Header";
 
 export const Layout = () => {
-  const navigate = useNavigate();
-  return (
-    <div className="layout">
-      <Header />
-      <div className="layout_content">
-        <Outlet />
-      </div>
-      <button onClick={() => navigate(-1)}>back</button>
-    </div>
-  );
+	const navigate = useNavigate();
+	return (
+		<div className="layout">
+			<Header />
+			<div className="layout_content">
+				<Outlet />
+			</div>
+			<button className="button_back" onClick={() => navigate(-1)}>
+				back
+			</button>
+			<footer
+				style={{ width: "100%", backgroundColor: "darkgrey", height: "45px" }}
+			>
+				<div style={{ textAlign: "center" }}>&copy; non-existant footer</div>
+			</footer>
+		</div>
+	);
 };
